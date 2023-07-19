a291662cdff0d6f2bf3fbdd6acea1f5d92307d8d
Shafique Dassu <shafd1231@gmail.com>
2023-07-19 23:46:57 +0100

    Reconfigured URL & function to the API Manager Included base URL as var in URL

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -28,71 +28,7 @@ class ViewController: UIViewController {
     }
     
     func updateWeather(lat: Double, long: Double) {
-        
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(lat)&lon=\(long)&appid=\(ApiManager.shared.apiKey)&units=metric") else {
-            print("DEBUG: url session error")
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            if let error = error {
-                print("DEBUG: Error fetching results \(error.localizedDescription)")
-                return
-            }
-            guard let data = data else {
-                print("DEBUG: Error getting data ")
-                return
-            }
-            do {
-                if let weatherData = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                    if let locationName = weatherData["name"] as? String {
-                        DispatchQueue.main.async {
-                            self.locationLabel.text = locationName
-                        }
-                    }
-                    
-                    if let weatherArray = weatherData["weather"] as? [[String: Any]] {
-                        let weather = weatherArray[0]
-                        if let description = weather["description"] as? String {
-                            DispatchQueue.main.async {
-                                self.weatherDescriptionLabel.text = description
-                            }
-                        }
-                        if let icon = weather["icon"] as? String {
-                            let iconString = "https://openweathermap.org/img/wn/" + icon + ".png"
-                            let iconURL = URL(string: iconString)
-                            self.weatherImageView.sd_setImage(with: iconURL, placeholderImage: UIImage(systemName: "person.fill"))
-                        }
-                    }
-                    if let mainData = weatherData["main"] as? [String: Any] {
-                        if let temperature = mainData["temp"] as? Double {
-                            let measurement = Measurement(value: temperature, unit: UnitTemperature.celsius)
-                            let measurementFormatter = MeasurementFormatter()
-                            measurementFormatter.unitStyle = .short
-                            measurementFormatter.numberFormatter.maximumFractionDigits = 0
-                            measurementFormatter.unitOptions = .temperatureWithoutUnit
-                            
-                            DispatchQueue.main.async {
-                                self.temperatureLabel.text = measurementFormatter.string(from: measurement)
-                            }
-                        }
-                        if let humidity = mainData["humidity"] as? Double {
-                            DispatchQueue.main.async {
-                                self.humidityLabel.text = String(humidity)
-                            }
-                        }
-                    }
-                    
-                    print(weatherData)
-                } else {
-                    print("DEBUG: could not convert to dictionary")
-                }
-                
-            }
-            catch {
-                print("Could not convert JSON data")
-            }
-        }
-        .resume()
+        ApiManager.shared.getWeather(lat: lat, long: long)
     }
 }
 
